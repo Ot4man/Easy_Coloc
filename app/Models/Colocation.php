4e4eb8c22726c6c8a4de80d8e7cb89c5295b26fb
Ot4man <melloukiotmane@gmail.com>
2026-02-24 14:50:00 +0000

    model: implement Colocation model and status management

--- app/Models/Colocation.php
+++ app/Models/Colocation.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Colocation extends Model
 {
-    protected $fillable = [ 'name','status' ];
+    protected $fillable = [ 'name','description','status' ];
 
 
     public function users()
@@ -27,4 +27,8 @@ class Colocation extends Model
     {
         return $this->hasMany(Invitation::class);
     }
+    public function categories()
+{
+    return $this->hasMany(Category::class);
+}
 }
